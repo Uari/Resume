ef62b2a667af3b9a68f71204b405851edb705304
pbh <bioqudgus@gmail.com>
2024-06-29 14:55:21 +0900

    start pj

---
+++ src/main/java/com/PBH/PbhApplication.java
@@ -0,0 +1,13 @@
+package com.PBH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PBH/PbhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PBH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
